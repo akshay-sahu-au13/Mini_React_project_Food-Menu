050079dd641811b483d9ad22189599aa08119763
Akshay Sahu <akki5233@gmail.com>
2021-04-18 00:31:50 +0530

    Basic layout done

--- food_menu/src/App.js
+++ food_menu/src/App.js
@@ -4,7 +4,24 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  return <>
+    <div className="container">
+      <div className="title">
+        <h2>Our Menu</h2>
+        <div className="underline"></div>
+      </div>
+
+      <div className="categories">
+
+      </div>
+
+      <div className="menu">
+        
+      </div>
+
+    </div>
+  
+  </>;
 }
 
 export default App;
